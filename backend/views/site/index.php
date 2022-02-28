c415d1876d306d9bf7a03a167aff8118dfcebae6
Bakhrom0409 <bahromislomov0904@gmail.com>
2022-02-28 21:57:38 +0500

    a

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -23,7 +23,7 @@ $this->title = 'My Yii Application';
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                    fugiat nulla pariatur.salom</p>
 
                 <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
